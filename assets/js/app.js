7ea6540e9f1e44d688cd0f66d66263c0de61ccc3
mirirosca <roscamiri@gmail.com>
2025-01-02 17:56:09 +0100

    change all event listener with event dele gation & code corrections

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,46 @@
 const counterContainer = document.getElementById('counter-container');
 const counterController = document.getElementById('counter-controller');
 
-let counterValue = 0
+let counterValue = 0;
 
-// Creation of counter's btns:
-// decrease, increase & reset button
-const counterLabel = document.createElement("label");
-counterLabel.id = "counter-label";
-counterLabel.textContent = counterValue;
+// Function to create DOM elements
+const createElement = (tag, id, textContent) => {
+  const element = document.createElement(tag);
+  if (id) element.id = id;
+  if (textContent) element.textContent = textContent;
+  return element;
+};
 
-const decreaseBtn = document.createElement("button");
-decreaseBtn.id = "decreaseBtn";
-decreaseBtn.textContent = "-"
-
-const increaseBtn = document.createElement("button");
-increaseBtn.id = "increaseBtn";
-increaseBtn.textContent = "+"
-
-const resetBtn = document.createElement("button");
-resetBtn.id = "resetBtn"
-resetBtn.textContent = "reset"
-
+const counterLabel = createElement("label", "counter-label", `${counterValue}`);
+const decreaseBtn = createElement('button', 'decreaseBtn', '-');
+const increaseBtn = createElement('button', 'increaseBtn', '+');
+const resetBtn = createElement('button', 'resetBtn', 'reset');
 
 // Function to update the value of the counter
 const updateCounter = () => {
   counterLabel.textContent = counterValue;
 };
 
-
-// Event listener for each btn
-decreaseBtn.addEventListener("click", () => {
-  counterValue--;
-  updateCounter();
-});
-
-increaseBtn.addEventListener("click", () => {
-  counterValue++;
-  updateCounter();
-})
-
-resetBtn.addEventListener("click", () => {
-  counterValue = 0;
-  updateCounter();
-});
-
-
 // Append elements to the DOM
 counterContainer.insertBefore(counterLabel, counterController);
 counterController.appendChild(decreaseBtn);
 counterController.appendChild(resetBtn);
 counterController.appendChild(increaseBtn);
+
+// Event delegation
+counterController.addEventListener('click', e => {
+  const clickedButton = e.target;
+
+  if (clickedButton.id === 'decreaseBtn') {
+    counterValue--;
+    updateCounter();
+  } else if (clickedButton.id === 'increaseBtn') {
+    counterValue++;
+    updateCounter();
+  } else if (clickedButton.id === 'resetBtn') {
+    counterValue = 0;
+    updateCounter();
+  }
+});
+
+
